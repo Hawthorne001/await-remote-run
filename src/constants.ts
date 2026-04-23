009e5689433128b4ff68aca7513799e3aaba11f6
Alex Miller <codex.nz@gmail.com>
2026-04-23 21:50:16 +1200

    refactor: more forgiving initial active job timeout

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 /* eslint-disable @typescript-eslint/no-inferrable-types */
 
-export const WORKFLOW_RUN_ACTIVE_JOB_TIMEOUT_MS: number = 1000;
+export const WORKFLOW_RUN_ACTIVE_JOB_TIMEOUT_MS: number = 10_000;
